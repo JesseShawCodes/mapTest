d5617721d74c59701af2231cea21db16fd8ad208
Jesse Shaw <jdshaw1987@gmail.com>
2018-07-11 11:40:44 -0400

    Got a LAT and LONG Return

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,24 @@ var key = "z9XYu29D14RwFZYrjn6p3b31SDQZMq27";
 
 $("form").submit(function(e){
     e.preventDefault()
-    var city = $(".city-input").val();
-    var url = `<img src="https://www.mapquestapi.com/staticmap/v5/map?key=${key}&center=${city}&size=500,500@2x">`;
-    $( ".result" ).html(url).show()
+    var streetAddress = $(".street-input").val().split(' ').join('+');
+    var city = $(".city-input").val().split(' ').join('+');
+    var state = $(".state-input").val().split(' ').join('+');
+    var zip = $(".zip-input").val();
+    var completeAddress = `${streetAddress}%2C+${city}%2C+${state}+${zip}`;
+    var cityReplaced = city.split(' ').join('+');
+    var url = `<img src="https://www.mapquestapi.com/staticmap/v5/map?key=${key}&center=${cityReplaced}&size=300,300@2x">`;
+    var metaUrl = `https://www.mapquestapi.com/geocoding/v1/address?key=${key}&inFormat=kvp&outFormat=json&location=${completeAddress}&thumbMaps=false`
+    $.get(`${metaUrl}`, function(data, status){
+        let lat = data.results["0"].locations["0"].latLng.lat
+        let lng = data.results["0"].locations["0"].latLng.lng
+        $(".result").html(`
+            lat: ${lat}, lng: ${lng}
+            <br>
+            ${url}
+        `)
+        console.log(`status: ${status}`)
+    });
+
 })
 
